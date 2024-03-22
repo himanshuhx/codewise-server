d368fcc8226d91266baca0607b452897e082b010
Himanshu Suman <himanshuhx1221@gmail.com>
2024-03-22 09:00:25 +0530

    fixed disconnect handling

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -2,6 +2,7 @@ import { OnModuleInit } from '@nestjs/common';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
@@ -14,11 +15,12 @@ import { Server, Socket } from 'socket.io';
     origin: '*',
   },
 })
-export class EventsGateway {
+export class EventsGateway implements OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
   userNameSocketIdMap = {};
+  socketIdToRoomId = {};
 
   getAllConnectedClients(roomId: string) {
     // return Array of connected clients with socketId and username
@@ -29,12 +31,18 @@ export class EventsGateway {
     );
   }
 
-  @SubscribeMessage('join')
+  @SubscribeMessage(ACTIONS.JOIN)
   onJoinEvent(@MessageBody() data: any, @ConnectedSocket() socket: Socket) {
     const { roomId, username } = data;
     this.userNameSocketIdMap[socket.id] = username;
+    if (this.socketIdToRoomId[socket.id]) {
+      this.socketIdToRoomId[socket.id].push(roomId);
+    } else {
+      this.socketIdToRoomId[socket.id] = [roomId];
+    }
     socket.join(roomId);
     const connectedClients = this.getAllConnectedClients(roomId);
+
     connectedClients.forEach((client) => {
       const clientSocketId = client.socketId;
       this.server.to(clientSocketId).emit(ACTIONS.JOINED, {
@@ -42,6 +50,21 @@ export class EventsGateway {
         socketId: socket.id,
         username,
       });
+    });
+  }
+
+  handleDisconnect(socket: Socket) {
+    console.log('cliend disconnected', socket.id);
+    // get All Rooms associated with current client
+    const rooms = this.socketIdToRoomId[socket.id];
+    // disconnect from each room
+    rooms.forEach((roomId) => {
+      this.server.to(roomId).emit(ACTIONS.DISCONNECTED, {
+        socketId: socket.id,
+        username: this.userNameSocketIdMap[socket.id],
+      });
+      delete this.userNameSocketIdMap[socket.id];
+      socket.leave(roomId);
     });
   }
 }
